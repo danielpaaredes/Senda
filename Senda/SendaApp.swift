6e366c30111f2d861a0148d5985c7c9f3fc0c132
danielparedess <danielparedesantana@gmail.com>
2026-04-21 20:43:20 -0600

    Initial Commit

---
+++ Senda/SendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendaApp.swift
+//  Senda
+//
+//  Created by Daniel Paredes on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
